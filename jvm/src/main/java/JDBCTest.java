f4fd73195cba04829fc1e520c0e7c1e76a9fb4d4
liuchang49507 <liuchang49507@hundsun.com>
2023-05-17 13:21:24 +0800

    add: compare 2 class equals

---
+++ jvm/src/main/java/JDBCTest.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "root");
+
+    }
+}
